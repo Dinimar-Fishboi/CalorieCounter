72af8cdd21a469432c413edfde72b1b8ee4ab3f2
Dinimar Fish <tayla@tallisman.com.au>
2021-12-20 11:50:29 +1000

    added REST routes to the Express Server

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
-
+app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.listen(8080, () => {
-  console.log('App listening on port 8080! Look at http://localhost:8080!')
+// store in memory
+let meals = []
+
+app.get('/meals', (req, res) => {
+  return res.send(meals)
 })
+
+app.post('/meals', (req, res) => {
+  let meal = req.body
+  meal.id = Date.now()
+  meals.push(meal)
+  return res.send(meal)
+})
+
+app.delete('/meals/:id', (req, res) => {
+  let id = parseInt(req.params.id)
+  const index = meals.map(o => o.id).indexOf(id)
+  if (index !== -1) {
+    meals.splice(index, 1)
+  }
+  return res.send()
+})
+
+app.listen(8080, () => {
+  console.log('App listening on port 8080!')
+})
+
+// GET /meals - Returns all meals
+// POST /meals - Add a new meal
+// DELETE /meals/:id - Removes a meal
